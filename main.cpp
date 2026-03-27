c65f39fe6c11e1417f72b58b1877eb3e67945c39
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-03-26 23:09:49 -0700

    Implement menu selection with switch statement
    Added a switch statement to handle user selection for menu options.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,29 @@ using namespace std;
 int main()
 {
        int selection;
-        cout << "Which formula do you want to see?\n\n";
-        cout << "1. Apple\n";
-        cout << "2. Bannana\n";
-        cout << "3. Orange\n" ;
-        cout << "4. None of them\n";
-        cin >> selection;
-// TODO
-// TODO
+       cout << "Which formula do you want to see?\n\n";
+       cout << "1. Apple\n";
+       cout << "2. Bannana\n";
+       cout << "3. Orange\n" ;
+       cout << "4. None of them\n";
+       cin >> selection;
+    
+       switch (selection) {
+        case 1:
+            cout << "Menu 1 selected\n";
+            break;
+        case 2:
+            cout << "Menu 2 selected\n";
+            break;
+        case 3:
+            cout << "Menu 3 selected\n";
+            break;
+        case 4:
+            cout << "Menu 4 selected\n";
+            break;
+        default:
+            cout << "Wrong menu item\n";
+    }
+
+    return 0;
 }
